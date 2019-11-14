5e2a2886449d5be13b6be49b327ed5d8fba0535e
Nathan-Marshall <nathsmar@gmail.com>
2019-11-14 13:45:46 -0500

    Create send and receive functions, implement some parts of the protocol

--- Alice.m
+++ Alice.m
@@ -5,6 +5,7 @@ classdef Alice
     properties
         K1
         K2
+        SCba
     end
     
     methods
@@ -13,48 +14,81 @@ classdef Alice
             obj.K2 = K2;
         end
         
-        function [] = sendMessage(obj, Bob, m)
+        function [] = sendMessage(obj, bob, m)
             %SENDMESSAGE Sends a message to Bob
-            %   Bob:Bob
-            %   m:string
+            %   in  bob:Bob - Recipient
+            %   in  m:string - message (cbit string)
             M = m + hash(m);
-            S = generateBellStates(M);
-            C = generateBellStates(randi([0 1], m.length, 1));
-            [Ca, Cb] = splitBellStates(C);
-            Q = LehmerReorder([dissolveBellStates(S) Cb], obj.K1);
+            S = generateBellPairs(M);
+            obj.C = generateBellPairs(randi([0 1], M.length, 1));
+            Cba = separateCheckPairs(obj.C);
+            obj.SCba = tensor(S, Cba);
+            Q = LehmerReorder(obj.SCba, obj.K1);
+            bob.receiveMessage(obj, Q);
+        end
+        
+        function [] = receiveReflectedCheckState(obj, bob, collapsedSCba_)
+            %RECEIVEREFLECTEDCHECKSTATE Receives the reflected Cb state
+            %       from Bob and reorders to bring the Bell-EPR pairs back
+            %       together, verifying with the original.
+            %   in  bob:Bob - Recipient of original message. Sender of the
+            %       reflected check state.
+            %   in  collapsedSCba_:state - The Cb portion of this state is
+            %       reflected by Bob. Ca was never actually sent by Alice,
+            %       and S was measured by Bob using Z-basis measurement.
+            
+            % TODO : Reorder reflected Cb based on K2, reassemble Bell-EPR
+            %        pairs, check validity.
         end
     end
     
     methods (Static)
         function [h] = hash(m)
-            %   h:string
-            %   m:string
+            %   in  m:string - input cbit string
+            %   out h:string - hashed value cbit string
             
         end
         
-        function [bellStates] = generateBellStates(cbits)
-            %   bellStates:[state]
-            %   cbits:string
-            bellStates = zeros(cbits.length/2, 1, state);
+        function [bellPairs] = generateBellPairs(cbits)
+            %   in  cbits:string - input cbit string for Bell-EPR pairs to
+            %       generate
+            %   out bellPairs:state - State containing all generated
+            %       Bell-EPR pairs tensored together
             for i = 1:2:cbits.length
-                bellStates((i+1)/2) = helper.ebit(state(cbits.substring(i, i+1)));
+                newPair = helper.ebit(state(cbits.substring(i, i+1)));
+                if exist('bellPairs', 'var')
+                    bellPairs = tensor(bellPairs, newPair);
+                else
+                    bellPairs = newPair;
+                end
             end
         end
         
-        function [qbits] = dissolveBellStates(bellStates)
-            %   QBits:[state]
-            %   bellStates:[state]
-            
+        function [Cba] = separateCheckPairs(C)
+            %   in  C:state - input qbits
+            %   out Cba:state - The same qbits reordered such that the
+            %       first qbit from every pair is pushed to the end. The
+            %       first n/2 qbits are considered Cb and the last n/2
+            %       qbits are considered Ca.
+            n = C.subsystems();
+            Cba = C;
+            for i = 1:n/2
+                % Move the first qbit from every pair to the end
+                swapToEnd = helper.BSWAP(i, n, n);
+                Cba = u_propagate(Cba, swapToEnd);
+            end
         end
         
-        function [firstQBits, secondQBits] = splitBellStates(bellStates)
-            %   firstQBits:[state]
-            %   secondQBits:[state]
-            %   bellStates:[state]
-            
-        end
-        
-        function [reorderedQBits] = LehmerReorder(qbits, key)
+        function [Q] = LehmerReorder(SCba, K1)
+            %   in  SCba:state - S is the tensored Bell-EPR pairs
+            %       representing the message and message hash components.
+            %       Cb is the sequence of 1st qbits from each of the check
+            %       state Bell-EPR pairs, and Ca is the 2nd qbit from each.
+            %   in  K1:string - Key used for reordering S+Cb according to
+            %       the Lehmer code algorithm.
+            %   out Q:state - Tensored and reordered output state. Ca stays
+            %       the same but S and Cb are shuffled together according
+            %       to K1.
             
         end
     end
--- Bob.m
+++ Bob.m
@@ -3,12 +3,69 @@ classdef Bob
     %   Bob has only classic capabilities and Z-basis measurement.
     
     properties
-        Property1
+        K1
+        K2
+        m_
     end
     
     methods
-        function obj = Bob(inputArg1,inputArg2)
-            obj.Property1 = inputArg1 + inputArg2;
+        function [] = receiveMessage(obj, alice, Q_)
+            %RECEIVEMESSAGE Receives a message from Alice
+            %   in  alice:Alice - Sender
+            %   in  Q:state - message as a quantum state containing the
+            %       message component S which is shuffled with the 2nd part
+            %       of the check component Cb, followed by the first part
+            %       of the check component Ca. Ca is technically not sent
+            %       by Alice and should not be touched by Bob in this
+            %       simulation.
+            SCba_ = LehmerReorder(Q_, obj.K1);
+            [M_, collapsedSCba_] = ReadMessage(SCba_);
+            [m_, hashVerified] = VerifyHash(M_);
+            if hashVerified
+                obj.m_ = m_;
+            else
+                % ERROR? Restart?
+            end
+            
+            % TODO: Reorder Cb based on K2
+            
+            alice.ReceiveReflectedCheckState(obj, collapsedSCba_);
+        end
+    end
+    
+    methods (Static)
+        function [SCba_] = LehmerReorder(Q_, K1)
+            %   in  Q:state - State as received from Alice (ignore Ca). We
+            %       will shuffle the S+Cb component back to its original
+            %       order.
+            %   in  K1:string - Key used for reordering S+Cb according to
+            %       the Lehmer code algorithm.
+            %   out Q_:state - Reordered output state. Ca stays the same
+            %       but S and Cb are shuffled back to their original order.
+            
+        end
+        
+        function [M_, collapsedSCba_] = ReadMessage(SCba_)
+            M_ = "";
+            n = SCba_.subsystems();
+            collapsedSCba_ = SCba_;
+            for i = 1:n/2
+                [~, b, collapsedSCba_] = measure(collapsedSCba_, i);
+                b = b - 1;
+                M_ = M_ + num2str(b, '%1d');
+            end
+        end
+        
+        function [h_] = hash(m_)
+            %   in  m_:string - input cbit string
+            %   out h_:string - hashed value cbit string
+            
+        end
+        
+        function [m_, success] = VerifyHash(M_)
+            h_ = M_.substring(1, M_.length/2);
+            m_ = M_.substring(M.length/2 + 1, M.length);
+            success = (h_ == hash(m_));
         end
     end
 end
